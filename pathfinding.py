408151a34dd69b2dd22d5c5c7d9a614db8087768
Mateusz Moroszczuk <moro143moro@gmail.com>
2021-04-02 12:50:48 +0200

    Added setting.py

--- pathfinding.py
+++ pathfinding.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pygame
+from settings import Settings
 
 class board():
-    def __init__(self, row, col, size=(500, 500), bgcolor = (0,0,0)):
+    def __init__(self):
+        self.settings = Settings()
+
         pygame.init()
-        self.screen = pygame.display.set_mode(size)
-
-        self.bgcolor = bgcolor
-        self.size = size
-
-        self.board = np.zeros([row, col])
+        self.screen = pygame.display.set_mode(self.settings.size)
+        self.board = np.zeros([self.settings.row, self.settings.col])
         self.aStar = []
         self.step = 0
         self.finish = None
@@ -22,35 +21,74 @@ class board():
             self.board[row][col] = 0
 
     def draw(self):
-        self.screen.fill(self.bgcolor)
-        
-        l = self.size[0]/len(self.board)
-        d = self.size[1]/len(self.board[0])
+        self.screen.fill(self.settings.bgcolor)
+        l = self.settings.size[0]/len(self.board)
+        d = self.settings.size[1]/len(self.board[0])
         for i in self.aStar:
-            pygame.draw.rect(self.screen, (100,0,0), (l*i[1], d*i[0], l, d))
+            pygame.draw.rect(self.screen, self.settings.acolor, (l*i[1], d*i[0], l, d))
         for i in self.path:
             if i != None:
-                pygame.draw.rect(self.screen, (200,0,0), (l*i[1], d*i[0], l, d))
+                pygame.draw.rect(self.screen, self.settings.pcolor, (l*i[1], d*i[0], l, d))
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
                 if self.board[i][j] == 1:
-                    pygame.draw.rect(self.screen, (200, 200, 200), (l*j, d*i, l,d))
+                    pygame.draw.rect(self.screen, self.settings.wcolor, (l*j, d*i, l,d))
                 elif self.board[i][j] == 2:
-                    pygame.draw.rect(self.screen, (0, 200, 200), (l*j, d*i, l,d))
+                    pygame.draw.rect(self.screen, self.settings.scolor, (l*j, d*i, l,d))
                 elif self.board[i][j] == 3:
-                    pygame.draw.rect(self.screen, (200, 200, 0), (l*j, d*i, l,d))
+                    pygame.draw.rect(self.screen, self.settings.fcolor, (l*j, d*i, l,d))
         pygame.display.update()
 
     def in_list(self, x, y):
-        good = True
-        if self.board[x][y] == 1:
-            good = False
-        for j in self.aStar:
-            if j[0]==x and j[1]==y:
+        if x>=0 and y>=0 and x<self.settings.row and y<self.settings.col:
+            good = True
+            if self.board[x][y] == 1:
                 good = False
-        if good:
-            self.aStar.append([x,y,self.step+1])
+            for j in self.aStar:
+                if j[0]==x and j[1]==y:
+                    good = False
+            if good:
+                self.aStar.append([x,y,self.step+1])
+    
+    def pathFind(self):
+        tmp = self.step
+        lista = [self.finish]
+        for i in range(tmp):
+            t = tmp-i
+            for j in self.aStar:
+                if t == j[2]:
+                    if j[0]==lista[-1][0]+1 and j[1]==lista[-1][1]:
+                        lista.append(j)
+                        break
+                    elif j[0]==lista[-1][0]-1 and j[1]==lista[-1][1]:
+                        lista.append(j)
+                        break
+                    elif j[0]==lista[-1][0] and j[1]==lista[-1][1]+1:
+                        lista.append(j)
+                        break
+                    elif j[0]==lista[-1][0] and j[1]==lista[-1][1]-1:
+                        lista.append(j)
+                        break
+        self.path = lista
 
+    def aStarFind(self):
+        if len(self.aStar) == 0:
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if self.board[i][j] == 2:
+                        self.aStar.append([i, j, 0])
+                        break
+                if len(self.aStar) != 0:
+                    break
+        else:
+            for i in self.aStar:
+                if i[2] == self.step:
+                    self.in_list(i[0]+1, i[1])
+                    self.in_list(i[0]-1, i[1])
+                    self.in_list(i[0], i[1]+1)
+                    self.in_list(i[0], i[1]-1)
+            self.step+=1
+            
     def run(self):
         running = True
         drawing = False
@@ -63,11 +101,11 @@ class board():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_s:
                         pos = pygame.mouse.get_pos()
-                        xy = [int(pos[0]/self.size[0]*len(self.board)), int(pos[1]/self.size[1]*len(self.board[0]))]
+                        xy = [int(pos[0]/self.settings.size[0]*len(self.board)), int(pos[1]/self.settings.size[1]*len(self.board[0]))]
                         self.board[xy[1], xy[0]] = 2
                     elif event.key == pygame.K_f:
                         pos = pygame.mouse.get_pos()
-                        xy = [int(pos[0]/self.size[0]*len(self.board)), int(pos[1]/self.size[1]*len(self.board[0]))]
+                        xy = [int(pos[0]/self.settings.size[0]*len(self.board)), int(pos[1]/self.settings.size[1]*len(self.board[0]))]
                         self.board[xy[1], xy[0]] = 3
                         self.finish = [xy[1], xy[0]]
                     elif event.key == pygame.K_SPACE:
@@ -75,7 +113,7 @@ class board():
                 elif event.type == pygame.MOUSEMOTION:
                     if drawing:
                         pos = pygame.mouse.get_pos()
-                        xy = [int(pos[0]/self.size[0]*len(self.board)), int(pos[1]/self.size[1]*len(self.board[0]))]
+                        xy = [int(pos[0]/self.settings.size[0]*len(self.board)), int(pos[1]/self.settings.size[1]*len(self.board[0]))]
                         if xy not in last:
                             self.change_board(xy[1], xy[0])
                             last.append(xy)
@@ -86,51 +124,16 @@ class board():
                     last = []
             
             if finding:
-                if len(self.aStar) == 0:
-                    for i in range(len(self.board)):
-                        for j in range(len(self.board[i])):
-                            if self.board[i][j] == 2:
-                                self.aStar.append([i, j, 0])
-                                print(self.aStar)
-                                break
-                        if len(self.aStar) != 0:
-                            break
-                else:
-                    for i in self.aStar:
-                        if i[2] == self.step:
-                            self.in_list(i[0]+1, i[1])
-                            self.in_list(i[0]-1, i[1])
-                            self.in_list(i[0], i[1]+1)
-                            self.in_list(i[0], i[1]-1)
-                    self.step+=1
+                self.aStarFind()
                 for i in self.aStar:
                     if i[0] == self.finish[0] and i[1] == self.finish[1]:
-                        print(i, self.finish)
                         finding = False
             else:
-                tmp = self.step
-                lista = [self.finish]
-                for i in range(tmp):
-                    t = tmp-i
-                    for j in self.aStar:
-                        if t == j[2]:
-                            if j[0]==lista[-1][0]+1 and j[1]==lista[-1][1]:
-                                lista.append(j)
-                                break
-                            elif j[0]==lista[-1][0]-1 and j[1]==lista[-1][1]:
-                                lista.append(j)
-                                break
-                            elif j[0]==lista[-1][0] and j[1]==lista[-1][1]+1:
-                                lista.append(j)
-                                break
-                            elif j[0]==lista[-1][0] and j[1]==lista[-1][1]-1:
-                                lista.append(j)
-                                break
-                self.path = lista
+                self.pathFind()
             self.draw()
 
 if __name__ == "__main__":
-    test = board(50,50)
+    test = board()
     test.change_board(0,1)
     print(test.board)
     test.run()
---
+++ settings.py
@@ -0,0 +1,13 @@
+
+class Settings():
+    def __init__(self):
+        self.size = (1000, 1000)
+        self.row = 100
+        self.col = 100
+
+        self.bgcolor = (0, 0, 0)
+        self.wcolor = (230, 230, 230)
+        self.pcolor = (200, 0, 0)
+        self.acolor = (0, 50, 0)
+        self.scolor = (61, 104, 245)
+        self.fcolor = (255, 255, 125)
